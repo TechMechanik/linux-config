44e4c306de10f34a1d89bed6d4a6cba828320865
Techmechanik <me@techmechanik.com>
2024-06-27 12:49:42 -0400

    Created file

---
+++ mountUnraid.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Create data share directories, if not exist
+sudo mkdir -p /srv/nfs/data/$USER
+sudo mkdir -p /home/$USER/Unraid/data
+
+# Create backup share directories, if not exist
+sudo mkdir -p /srv/nfs/backup/$USER
+sudo mkdir -p /home/$USER/Unraid/backup
+
+# Mount Unraid NFS
+sudo mount -t nfs darktower.local:/mnt/user/backups /srv/nfs/backup/eric
+sudo mount -t nfs darktower.local:/mnt/user/data /srv/nfs/data/eric
+
+# Create symlinks to home directory
+ln -s /srv/nfs/backup/$USER /hone/$USER/Unraid/backup
+ln -s /srv/nfs/data/$USER /home/$USER/Unraid/data
+

314fdffc83ef55e610ff346d880b599b82ff7c54
TechMechanik <1356227+TechMechanik@users.noreply.github.com>
2024-06-27 16:34:47 -0400

    Create createHybridDrive.sh

---
+++ createHybridDrive.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Wipe drives
+sudo wipefs --all --backup /dev/sda
+sudo wipefs --all --backup /dev/sdb
+
+# create 
+sudo pvcreate /dev/sda
+sudo pvcreate /dev/sdb
+# sudo nano /etc/lvm/lvm.conf
+sudo vgcreate vg_data /dev/sda /dev/sdb
+sudo lvcreate -L 10G -n lv_data vg_data /dev/sda
+sudo lvcreate -n cachepool_meta -L 100M vg_data /dev/sdb
+sudo lvcreate -n cachepool -L 5G vg_data /dev/sdb
+sudo lvconvert --type cache-pool --poolmetadata cachepool_meta vg_data/cachepool
+sudo lvconvert --type cache --cache-pool cachepool --cachemode writethrough vg_data/lv_data

7e22e10dfb6104d1860c0932e2074b0cb2c00dd7
TechMechanik <1356227+TechMechanik@users.noreply.github.com>
2024-06-27 17:04:28 -0400

    Update createHybridDrive.sh

--- createHybridDrive.sh
+++ createHybridDrive.sh
@@ -14,3 +14,8 @@ sudo lvcreate -n cachepool_meta -L 100M vg_data /dev/sdb
 sudo lvcreate -n cachepool -L 5G vg_data /dev/sdb
 sudo lvconvert --type cache-pool --poolmetadata cachepool_meta vg_data/cachepool
 sudo lvconvert --type cache --cache-pool cachepool --cachemode writethrough vg_data/lv_data
+
+# To remove the cache
+# lvconvert --uncache vg_data/lv_data
+
+sudo lvextend -l +100%FREE /dev/vg_data/lv_data
